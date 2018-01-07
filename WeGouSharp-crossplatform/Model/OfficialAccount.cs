fbeb76e98ddb5d473242189d9add3112e4b8bcab
罗海豪 <luohaihao@gmail.com>
2018-01-07 20:05:05 +0800

    add model

---
+++ WeGouSharp-crossplatform/Model/OfficialAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace WeGouSharpPlus.Model
+{
+    /// <summary>
+    /// 公众号
+    /// </summary>
+    class OfficialAccount
+    {
+        public string AccountPageurl { get; set; }
+
+        public string WeChatId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Introduction { get; set; }
+
+        /// <summary>
+        /// //是否官方认证账号
+        /// </summary>
+        public bool IsAuth { get; set; } 
+
+        public string QrCode { get; set; }
+
+        // public string Profile;
+        public string ProfilePicture { get; set; }
+
+        public string RecentArticleUrl { get; set; }
+    }
+}
---
+++ WeGouSharp-crossplatform/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeGouSharp_crossplatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
