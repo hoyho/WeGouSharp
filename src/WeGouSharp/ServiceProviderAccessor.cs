4cd7f3fe8ca86d4bb82a7a9e4263bbfd9b69da16
Hoyho Luo <luohaihao@gmail.com>
2018-05-26 21:23:51 +0800

    use dependence injection

---
+++ src/WeGouSharp/ServiceProviderAccessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WeGouSharp
+{
+    public static class ServiceProviderAccessor
+    {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        public static void SetServiceProvider(IServiceProvider sp)
+        {
+            ServiceProvider = sp;
+        }
+    }
+}
